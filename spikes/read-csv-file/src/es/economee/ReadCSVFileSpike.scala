4fe78a3ca45b292f013c35c87f952a09b2774657
Ruben Chavarria <txingo@gmail.com>
2013-06-03 16:50:47 +0200

    learning how to read from a file in scala

---
+++ spikes/read-csv-file/src/es/economee/ReadCSVFileSpike.scala
@@ -0,0 +1,10 @@
+package es.economee
+
+object ReadCSVFileSpike {
+
+  def main(args:Array[String]):Unit = {
+    println("Reading a CVS file and printing its content to the console")
+    
+    
+  }
+}
