a82eb035ff9aeed4f30d8667ace0c5c71ff649a0
Ruben Chavarria <txingo@gmail.com>
2013-06-06 15:23:00 +0200

    starting to parse an example with Bankia movements

--- spikes/read-csv-file/src/es/economee/ReadCSVFileSpike.scala
+++ spikes/read-csv-file/src/es/economee/ReadCSVFileSpike.scala
@@ -5,13 +5,23 @@ import scala.io.Source
 object ReadCSVFileSpike {
 
   def main(args:Array[String]):Unit = {
-    val filePath = "test/example.csv"
+    val filePath = "test/bankia-example.csv"
     println("Reading CVS file: " + filePath)
     
-    val lines = Source.fromFile(filePath).getLines
-    for(line <- lines) {
-      val fields = List.fromArray(line.split(";"))
-      println(fields)
+    val rawLines = Source.fromFile(filePath).getLines
+    val infoLines = rawLines.drop(5) // skip first 4 lines
+    val movementLines = infoLines.filter(_.length > 0)
+    
+    for(line <- movementLines) {
+      val translatedLine = line.replace("\",\"", "\";\"")
+      val fields = List.fromArray(translatedLine.split(";"))
+      println("line contains " + fields.length + " fields {" + fields + "}")
+      
+      println("Movement date: " + getDate(fields));
     }
+  }
+  
+  private def getDate(fields:List[String]):String = {
+    fields(1).replace("\"", "")
   }
 }
