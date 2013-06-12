7dd1a47068cfc24b3879cd788915d52c90a94820
Ruben Chavarria <txingo@gmail.com>
2013-06-12 15:22:40 +0200

    first test, learning scalatest

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ economee
 ========
 
 Economee, an application to track your past, present and future economy
+
+Frameworks, libraries used
+==========================
+
+- [ScalaTest](http://scalatest.org/user_guide/matchers_quick_reference)
---
+++ spikes/read-csv-file/test/es/economee/ReadCSVFileSpikeTest.scala
@@ -0,0 +1,16 @@
+package es.economee
+
+import scala.collection.Seq
+import org.scalatest.FunSuite
+import scala.collection.immutable.Map
+import scala.reflect.Manifest
+import scala.runtime.BoxedUnit
+import java.lang.reflect.Method
+import org.scalatest.matchers.ShouldMatchers
+
+class ReadCSVFileSpikeTest extends FunSuite with ShouldMatchers {
+
+  test("This is my first test") {
+    "foo" should be("foo")
+  }
+}
