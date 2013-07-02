85b185a3fe50e6303b43d857005c99cb31edf3b0
Ruben Chavarria <txingo@gmail.com>
2013-07-02 13:03:14 +0200

    added some actions to list tasks, add new task, and delete task; they're not implemented yet

--- spikes/todolist/app/controllers/Application.scala
+++ spikes/todolist/app/controllers/Application.scala
@@ -8,5 +8,11 @@ object Application extends Controller {
   def index = Action {
     Ok("Welcome to my first Play app")
   }
+
+  def tasks = TODO
   
+  def newTask = TODO
+  
+  def deleteTask(id: Long) = TODO
+    
 }
