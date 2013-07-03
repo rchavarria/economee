9f04e718e6a81e98de8accaf0289214fe031d037
Ruben Chavarria <txingo@gmail.com>
2013-07-03 15:28:00 +0200

    created model class 'Task' and implemented actions listTasks, createTask. modified index.html view

--- spikes/todolist/app/controllers/Application.scala
+++ spikes/todolist/app/controllers/Application.scala
@@ -1,7 +1,11 @@
 package controllers
 
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
+
+import models._
 
 object Application extends Controller {
   
@@ -10,10 +14,24 @@ object Application extends Controller {
     Redirect(routes.Application.tasks)
   }
 
-  def tasks = TODO
+  def tasks = Action {
+  	Ok(views.html.index(Task.all(), taskForm))
+  }
   
-  def newTask = TODO
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
   
   def deleteTask(id: Long) = TODO
-    
+  
+  // define a form for Task's
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
 }
