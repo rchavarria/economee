34280d86af8a4805f35011d8a9bb6c3faffd5fc0
Ruben Chavarria <txingo@gmail.com>
2013-07-17 13:12:34 +0200

    split evolutions scripts into these 3: task table, movement table, insert data into movement table

--- spikes/todolist/conf/evolutions/default/1.sql
+++ spikes/todolist/conf/evolutions/default/1.sql
@@ -8,21 +8,7 @@ CREATE TABLE task (
     label VARCHAR(255)
 );
 
-CREATE SEQUENCE movement_id_seq;
-CREATE TABLE movement (
-	id INT NOT NULL DEFAULT nextval('movement_id_seq'),
-	concept VARCHAR(255) NOT NULL,
-	date BIGINT NOT NULL,
-	amount BIGINT NOT NULL
-);
-
-INSERT INTO movement (concept, date, amount) VALUES ('primer conceto', 1122334455, 12345);
-INSERT INTO movement (concept, date, amount) VALUES ('secondo conceto', 1122334457, 54321);
-
 # --- !Downs
  
 DROP TABLE task;
 DROP SEQUENCE task_id_seq;
-
-DROP TABLE movement;
-DROP SEQUENCE movement_id_seq;
